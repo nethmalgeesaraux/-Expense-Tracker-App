2550eb7c427df45162408962b22c441f3e346e49
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-03 15:21:55 +0530

    install Library

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import "../global.css";
+import { View, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>WellCome Nethmal</Text>
+      <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
