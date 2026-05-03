783ee23f6acfc3f15f825dfa7deddfaa1e28561d
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-05-03 15:46:34 +0530

    Update project structure and configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,9 @@
-import "../global.css";
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-      <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
+    <View >
+      <Text className="text-xl font-bold text-blue-500">Welcome to Nethmal!</Text>
     </View>
   );
 }
